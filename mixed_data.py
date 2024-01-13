aff6ea241f79687372a70e6ac91d0b4582b9adff
Mudasir24 <151111505+Mudasir24@users.noreply.github.com>
2024-01-13 12:38:17 +0530

    Add files via upload

---
+++ mixed_data.py
@@ -0,0 +1,11 @@
+# Declaring a list
+mixed_data = [85,"Mudasir","Mango",15.84,"India",3+2j,"MSS",12,True,3.14159]
+
+# Printing first 3 elements
+print("The first three elements are ",mixed_data[0:3])
+
+# Printing elements starting from the 2nd to the 5th position
+print("Elements starting from the 2nd to the 5th position are ",mixed_data[1:5])
+
+# Printing the last two elements
+print("The last two elements are ",mixed_data[8:10])
